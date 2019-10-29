f9e2dfba802aa80a6d321b625311f0b71b9be933
Aidan <akinzett@gmail.com>
2019-10-29 15:53:36 +1000

    changed version required

--- setup.py
+++ setup.py
@@ -12,20 +12,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-gdal_package = 'pygdal'
-try:
-    gdal_version = subprocess.check_output(
-        'gdal-config --version',
-        stderr=subprocess.STDOUT,
-        shell=True
-    ).decode('utf-8').strip()
-
-    if gdal_version.startswith('1.10'):
-        gdal_package = gdal_package + '==1.10.0'
-    else:
-        gdal_package = '%s==%s' % (gdal_package, gdal_version)
-except subprocess.CalledProcessError:
-    gdal_version = None
+gdal_package = 'pygdal==3.0.1.5'
 
 requirements = [gdal_package]
 
@@ -34,8 +21,8 @@ setup_requirements = ['pytest-runner', ]
 test_requirements = ['pytest', ]
 
 setup(
-    author="Tehamalab",
-    author_email='developers@tehamalab.com',
+    author="National Drones",
+    author_email='smartdata@nationaldrones.com',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
@@ -48,13 +35,13 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-    description="A python library for generating map tiles based on gdal2tiles.py script.",
+    description="A fork of gdal2tiles using pygdal so that it can be installed in a virtual environment. A python library for generating map tiles based on gdal2tiles.py script.",
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='gdal2tiles',
-    name='gdal2tiles',
+    name='pygdal2tiles',
     packages=find_packages(include=['gdal2tiles']),
     setup_requires=setup_requirements,
     extras_require={
@@ -62,7 +49,7 @@ setup(
     },
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/tehamalab/gdal2tiles',
+    url='https://github.com/nationaldrones/gdal2tiles',
     version='0.1.5',
     zip_safe=False,
 )
